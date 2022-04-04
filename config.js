8f745301d85f880eaa48c43601a25a44608e7673
LinhHap <102961551+LinhHap@users.noreply.github.com>
2022-04-04 18:40:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Tró Hấp",
+    introTitle: 'Em có yêu anh không <3',
+    introDesc: `Thử nói không yêu xem anh có đốt nhà không`,
+    btnIntro: '^^Chụt Chụt^^',
+    title: 'Và một ngày anh sẽ đến vì nụ cười em đấy thôi 🥰',
+    desc: 'Gần kề ta đi những nơi xa vời chỉ hai chúng ta ben đời ',
+    btnYes: 'Yêu anh nhiều lắm ạ <3',
+    btnNo: 'Ghét vailon :3',
+    question: 'Sao bé lại yêu anh <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh đoán được ngay mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Làm người yêu anh nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
