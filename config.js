6fa6fd588ced1c9106608668ed5834357d1ce275
LinhHap <102961551+LinhHap@users.noreply.github.com>
2022-04-04 18:32:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tró Hấp <3",
     introTitle: 'Em có yêu anh không?',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    introDesc: `Thử nói không yêu đi xem mình có đốt nhà bạn không :v `,
+    btnIntro: '^^Chụt chụt^^',
     title: 'Và một ngày anh sẽ đến vì nụ cười em đấy thôi 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    desc: 'Gần kề ta đi những nơi va vời chỉ hai chúng ta bên đời ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnNo: 'Không ai khác ngoài anh :3',
+    question: 'Sao tró lại yêu anh vậy ạ <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Anh đoán dược ngay mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Nhận lời làm người yêu nha hí hí.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
