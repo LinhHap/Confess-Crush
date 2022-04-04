7660c4c96c57639fffaa0275ed240a9972a875a5
LinhHap <102961551+LinhHap@users.noreply.github.com>
2022-04-04 18:57:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Anh đoán dược ngay mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Nhận lời làm người yêu nha hí hí.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100010380187840' 
 }
